6889cbd85dd5be5d7012e8fe75d88ad5ac4f31d2
Md Zikrullah <mdzikrullah042004@gmail.com>
2026-07-20 20:02:46 +0530

    refactor: improve layout structure and type annotations in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,33 @@
-import "./globals.css";
+import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
+import "./globals.css";
+
 import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer"
+import Footer from "./Components/Footer";
 
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["300", "400", "500", "700"],
   variable: "--font-roboto",
+  display: "swap",
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Zikks",
   description: "Zikks Website",
 };
 
-
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <Navbar />
-        {children}
-        <Footer/>
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
